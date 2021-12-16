60da385e10f4e373d44980fa4c74476fa6aa7f30
yyf <523225812@qq.com>
2021-12-16 23:29:58 +1030

    1.init project by vue-cli. 2. add less & less loader & style-resource loader. 3. add fonts. 4 add header compoents

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <h3 class="title-bar">Brightness & Contrast Developer Test</h3>
+    <h4 class="time-bar">01 Jun, 2018 – 31 Dec, 2019</h4>
+  </div>
+</template>
+<style lang="less" scoped>
+.header {
+  height: 70px;
+  text-align: center;
+  background-color: @purple;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .title-bar,
+  .time-bar {
+    margin: 0;
+  }
+  .title-bar {
+    font-size: @font-size-normal;
+    font-family: @font-Medium;
+    line-height: 25px;
+  }
+  .time-bar {
+    font-size: @font-size-medium;
+    font-family: @font-Regular;
+    line-height: 20px;
+  }
+}
+</style>
