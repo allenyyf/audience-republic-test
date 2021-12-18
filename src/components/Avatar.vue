d89073cb6bf37d418039e9a4ef7cbf5d93a3d9eb
yyf <523225812@qq.com>
2021-12-18 10:31:53 +1030

    add avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,52 @@
+<template>
+  <div :style="setStyle()" class="avatar-container">
+    <img :src="imageUrl" alt="" />
+  </div>
+</template>
+<script>
+export default {
+  name: "avatar",
+  props: {
+    width: {
+      type: Number,
+      default: 60,
+    },
+    height: {
+      type: Number,
+      default: 60,
+    },
+    isCircle: {
+      type: Boolean,
+      default: true,
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    setStyle() {
+      let styleObj = {
+        width: this.width + "px",
+        height: this.height + "px",
+      };
+      if (this.isCircle) {
+        styleObj["borderRadius"] = this.width + "px";
+      }
+      return styleObj;
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.avatar-container {
+  background-color: #e5e5e5;
+  border: 2px solid #fff;
+  overflow: hidden;
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,32 +1,78 @@
 <template>
   <div class="header">
-    <h3 class="title-bar">Brightness & Contrast Developer Test</h3>
-    <h4 class="time-bar">01 Jun, 2018 – 31 Dec, 2019</h4>
+    <div class="text-container">
+      <h3 class="title-bar">Brightness & Contrast Developer Test</h3>
+      <h4 class="time-bar">01 Jun, 2018 – 31 Dec, 2019</h4>
+    </div>
+    <div class="image-container">
+      <img class="cover-image" :src="coverImage" alt="" />
+      <div class="linear-bg"></div>
+      <Avatar class="avatar" :imageUrl="avatarImage"></Avatar>
+    </div>
   </div>
 </template>
+<script>
+import Avatar from "@/components/Avatar";
+export default {
+  components: { Avatar },
+  data() {
+    return {
+      coverImage: require("@/assets/images/cover-image.png"),
+      avatarImage: require("@/assets/images/avatar.png"),
+    };
+  },
+};
+</script>
 <style lang="less" scoped>
 .header {
-  height: 70px;
-  text-align: center;
   background-color: @purple;
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  .title-bar,
-  .time-bar {
-    margin: 0;
+  .text-container {
+    text-align: center;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 70px;
+    .title-bar,
+    .time-bar {
+      margin: 0;
+    }
+    .title-bar {
+      font-size: @font-size-normal;
+      font-family: @font-Medium;
+      line-height: 25px;
+    }
+    .time-bar {
+      font-size: @font-size-medium;
+      font-family: @font-Regular;
+      line-height: 20px;
+    }
   }
-  .title-bar {
-    font-size: @font-size-normal;
-    font-family: @font-Medium;
-    line-height: 25px;
-  }
-  .time-bar {
-    font-size: @font-size-medium;
-    font-family: @font-Regular;
-    line-height: 20px;
+  .image-container {
+    display: flex;
+    flex-direction: column;
+    // justify-content: center;
+    align-items: center;
+    height: 195px;
+    position: relative;
+    .cover-image {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+    .linear-bg {
+      position: absolute;
+      bottom: 0;
+      height: 133px;
+      width: 100%;
+      opacity: 0.3;
+      background: linear-gradient(180deg, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1));
+    }
+    .avatar {
+      position: absolute;
+      bottom: -10px;
+    }
   }
 }
 </style>
